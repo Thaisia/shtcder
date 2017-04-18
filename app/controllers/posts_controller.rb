6f4873fe53974bf416f0c11872d3d56f98250bf9
Taisia Bo <thaisbox@gmail.com>
2017-04-19 01:18:58 +0300

    creating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
-  def new    
+
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to @post
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
