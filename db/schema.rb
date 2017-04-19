695208d6125871b0888d9c93ad2b2b4adf2f8676
Taisia Bo <thaisbox@gmail.com>
2017-04-19 23:35:15 +0300

    public/private post implementation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419190547) do
+ActiveRecord::Schema.define(version: 20170419202413) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170419190547) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.boolean  "public"
   end
 
   create_table "users", force: :cascade do |t|
