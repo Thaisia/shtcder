53d770a14d40b3f20aa03a298ac22342bb715235
Taisia Bo <thaisbox@gmail.com>
2017-04-19 22:10:22 +0300

    comments references

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ActiveRecord::Base
-    has_many :comments
+
+  validates :title, presence: true
+  validates :text, presence: true
+  
+  has_many :comments
+  belongs_to :user
+
+  def subject
+    title
+  end
+
+  def last_comment
+    comments.last
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419184919) do
+ActiveRecord::Schema.define(version: 20170419190547) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text"
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
@@ -27,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170419184919) do
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
