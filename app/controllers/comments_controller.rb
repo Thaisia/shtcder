46dd923bfca0756a5db9ea0a025ddd2e147c625b
Taisia Bo <thaisbox@gmail.com>
2017-04-25 21:30:48 +0300

    moving comment params to private

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,14 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @post.comments.create({user_id: current_user.id, text: params[:comment]['text'] })
+    @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:text)
+    params.require(:comment).permit(:text).merge(user_id:  current_user.id)
   end
 
 end
